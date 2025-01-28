9fc8e7b3ae58b5c831115df73426df1ca89973af
ProgrammerIn-wonderland <3838shah@gmail.com>
2025-01-28 17:32:38 -0500

    jsftp put working, fully functional and the code quality is abysmal

--- example.js
+++ example.js
@@ -2,10 +2,10 @@
 
 (async () => {
   const ftp = new window.jsftp({
-    host: 'cygwin.mirror.rafal.ca',
+    host: 'ftp.dlptest.com',
     port: 21,
-    user: 'ftp',
-    pass: 'email@example.com',
+    user: 'dlpuser',
+    pass: 'rNrKYTX9g7z3RgJRmxWuGHbeu',
   });
 
   try {
@@ -17,7 +17,7 @@
     console.log('Listing files...');
     const fileList = await ftp.list('.');
     console.log('Files:', fileList);
-    await ftp.put(new TextEncoder().encode("Testing puter-ftp"), "/hello.txt");
+    await ftp.put(Buffer.from(new TextEncoder().encode("Testing puter-ftp")), "/hello1.txt");
   } catch (error) {
     console.error('An error occurred:', error);
   } finally {
